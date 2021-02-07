26d3c3ea744ed0465504f559ec3f0a4c023272a2
ROBIN <robincuetcse@gmail.com>
2021-02-08 01:39:16 +0600

    Frontend fixed successfully

---
+++ frontend/src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Container} from 'react-bootstrap';
+import './App.css';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+import CartScreen from './screens/CartScreen';
+import LoginScreen from './screens/LoginScreen';
+import RegisterScreen from './screens/RegisterScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+        <Route path='/profile' component={ProfileScreen} />
+        <Route path='/register' component={RegisterScreen} />
+        <Route path='/login' component={LoginScreen} />
+        <Route path='/product/:id' component={ProductScreen} />
+        <Route path='/cart/:id?' component={CartScreen} /> {/* here ? is use for making it optional.Like...when we direct click the cart from our header we don't have any id */}
+        <Route path='/' component={HomeScreen} exact />
+
+         
+        </Container>
+      </main>
+      <Footer/>
+      </Router>
+   
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/screens/HomeScreen.js
@@ -0,0 +1,47 @@
+import React,{useEffect} from 'react';
+import {  Row,Col } from 'react-bootstrap';
+import { useDispatch ,useSelector} from 'react-redux';
+import { listProducts } from '../actions/productActions';
+import Loader from '../components/Loader';
+import Message from '../components/Message';
+import Product from '../components/Product';
+
+
+
+function HomeScreen() {
+  
+    const dispatch = useDispatch()
+
+   
+    const productList = useSelector((state) => state.productList)   //geting the needed state from REDUX
+    const { loading, error, products } = productList
+
+   
+ 
+    useEffect(()=>{
+       dispatch(listProducts())
+    },[dispatch])
+
+  
+
+
+    return (
+        <>
+         <h1>Latest Products</h1>
+         {loading?( <Loader/>):error? (<Message variant='danger'>{error}</Message>) : 
+         (
+            <Row>
+            {products.map(product=>(
+                <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                     <Product product={product}/>
+                </Col>
+            ))}
+        </Row>
+         )
+         }
+   
+        </>
+    )
+}
+
+export default HomeScreen;
